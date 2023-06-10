3de897bf3879e94aeffc1f9b9b05c6d8d93fe1c1
SAURABH NEGI <saurabhnegi.35@gmail.com>
2023-06-10 21:27:13 +0530

    Added Routes for Post

---
+++ controllers/postController.js
@@ -0,0 +1,5 @@
+const Post = require("../models/Post.js")
+
+exports.getFeedPosts = () => {
+
+}
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -10,5 +10,10 @@ const { verifyToken } = require("../middleware/auth");
 const router = express.Router();
 
 /* READ */
+router.get("/", verifyToken, getFeedPosts);
+router.get("/:userId/posts", verifyToken, getUserPosts);
 
 /* UPDATE */
+router.patch("/:id/like", verifyToken, likePost);
+
+module.exports = router;
